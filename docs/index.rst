9dba29ebf8dafe02b221836c0cf4f48e15365379
brgcode <vanmelet@ethz.ch>
2021-03-22 09:36:32 +0100

    base setup

---
+++ docs/index.rst
@@ -0,0 +1,32 @@
+********************************************************************************
+compas_occ
+********************************************************************************
+
+.. rst-class:: lead
+
+This is the documentation of COMPAS OCC.
+
+.. .. figure:: /_images/view2_scripted.png
+     :figclass: figure
+     :class: figure-img img-fluid
+
+
+Table of Contents
+=================
+
+.. toctree::
+   :maxdepth: 3
+   :titlesonly:
+
+   Introduction <self>
+   gettingstarted
+   tutorial
+   api
+   license
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
---
+++ src/compas_occ/__init__.py
@@ -0,0 +1,36 @@
+"""
+********************************************************************************
+compas_occ
+********************************************************************************
+
+.. currentmodule:: compas_occ
+
+
+.. toctree::
+    :maxdepth: 1
+
+
+"""
+
+from __future__ import print_function
+
+import os
+import sys
+
+
+__author__ = ["tom van mele"]
+__copyright__ = "Block Research Group - ETH Zurich"
+__license__ = "MIT License"
+__email__ = "van.mele@arch.ethz.ch"
+__version__ = "0.1.0"
+
+
+HERE = os.path.dirname(__file__)
+
+HOME = os.path.abspath(os.path.join(HERE, "../../"))
+DATA = os.path.abspath(os.path.join(HOME, "data"))
+DOCS = os.path.abspath(os.path.join(HOME, "docs"))
+TEMP = os.path.abspath(os.path.join(HOME, "temp"))
+
+
+__all__ = ["HOME", "DATA", "DOCS", "TEMP"]
