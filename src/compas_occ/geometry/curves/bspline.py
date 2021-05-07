7b924a22e52b637fd70faf2237b82d99c62dd670
brgcode <vanmelet@ethz.ch>
2021-05-07 11:42:25 +0200

    flexible construction methods

--- src/compas_occ/geometry/curves/bspline.py
+++ src/compas_occ/geometry/curves/bspline.py
@@ -9,112 +9,104 @@ from compas_occ.interop.arrays import array_of_points, array_of_floats, array_of
 from OCC.Core.gp import gp_Trsf
 from OCC.Core.Geom import Geom_BSplineCurve
 from OCC.Core.GeomAPI import GeomAPI_Interpolate, GeomAPI_PointsToBSpline
+from OCC.Core.TColgp import TColgp_Array1OfPnt
+from OCC.Core.TColStd import TColStd_Array1OfReal, TColStd_Array1OfInteger
 
 
 class BSplineCurve:
     """Wrapper for OCC BSplineCurve."""
 
-    def __init__(self,
+    def __init__(self) -> BSplineCurve:
+        self.occ_curve = None
+
+    def __eq__(self, other: BSplineCurve) -> bool:
+        return self.occ_curve.IsEqual(other.occ_curve)
+
+    @classmethod
+    def from_parameters(cls,
                  poles: List[Point],
                  knots: List[float],
                  multiplicities: List[int],
                  degree: int,
                  is_periodic: bool = False) -> BSplineCurve:
-        self._occ_curve = Geom_BSplineCurve(
+        curve = cls()
+        curve.occ_curve = Geom_BSplineCurve(
             array_of_points(poles),
             array_of_floats(knots),
             array_of_integers(multiplicities),
             degree,
             is_periodic,
         )
+        return curve
 
     @classmethod
     def from_interpolation(cls, points: List[Point]) -> BSplineCurve:
-        occ_curve = GeomAPI_Interpolate(array_of_points(points)).Curve()
-        poles = [Point(pole.X(), pole.Y(), pole.Z()) for pole in occ_curve.Poles()]
-        knots = [float(k) for k in occ_curve.Knots()]
-        multiplicities = [int(m) for m in occ_curve.Multiplicities()]
-        degree = occ_curve.Degree()
-        is_periodic = occ_curve.IsPeriodic()
-        return cls(poles, knots, multiplicities, degree, is_periodic)
+        curve = cls()
+        curve.occ_curve = GeomAPI_Interpolate(array_of_points(points)).Curve()
+        return curve
 
     @classmethod
     def from_points(cls, points: List[Point]) -> BSplineCurve:
-        occ_curve = GeomAPI_PointsToBSpline(array_of_points(points)).Curve()
-        poles = [Point(pole.X(), pole.Y(), pole.Z()) for pole in occ_curve.Poles()]
-        knots = [float(k) for k in occ_curve.Knots()]
-        multiplicities = [int(m) for m in occ_curve.Multiplicities()]
-        degree = occ_curve.Degree()
-        is_periodic = occ_curve.IsPeriodic()
-        return cls(poles, knots, multiplicities, degree, is_periodic)
+        curve = cls()
+        curve.occ_curve = GeomAPI_PointsToBSpline(array_of_points(points)).Curve()
+        return curve
 
     @classmethod
-    def from_step(cls, filepath):
+    def from_step(cls, filepath) -> BSplineCurve:
         pass
 
-    def to_step(self, filepath):
+    def to_step(self, filepath) -> None:
         pass
 
-    def __eq__(self, other: BSplineCurve) -> bool:
-        return self._occ_curve.IsEqual(other._occ_curve)
+    @property
+    def occ_poles(self) -> TColgp_Array1OfPnt:
+        return self.occ_curve.Poles()
 
     @property
-    def _poles(self):
-        return self._occ_curve.Poles()
+    def poles(self) -> List[Point]:
+        return [Point(pole.X(), pole.Y(), pole.Z()) for pole in self.occ_poles]
 
     @property
-    def poles(self):
-        return [Point(pole.X(), pole.Y(), pole.Z()) for pole in self._poles]
+    def occ_knots(self) -> TColStd_Array1OfReal:
+        return self.occ_curve.Knots()
 
     @property
-    def _weights(self):
-        return self._occ_curve.Weights()
+    def knots(self) -> List[float]:
+        return self.occ_knots
 
     @property
-    def weights(self):
-        return self._weights
+    def occ_multiplicities(self) -> TColStd_Array1OfInteger:
+        return self.occ_curve.Multiplicities()
 
     @property
-    def _knots(self):
-        return self._occ_curve.Knots()
-
-    @property
-    def knots(self):
-        return self._knots
-
-    @property
-    def _multiplicities(self):
-        return self._occ_curve.Multiplicities()
-
-    @property
-    def multiplicities(self):
-        return self._multiplicities
+    def multiplicities(self) -> List[int]:
+        return self.occ_multiplicities
 
     @property
     def degree(self) -> int:
-        return self._occ_curve.Degree()
+        return self.occ_curve.Degree()
 
     @property
     def start(self) -> Point:
-        pnt = self._occ_curve.StartPoint()
+        pnt = self.occ_curve.StartPoint()
         return Point(* pnt.XYZ())
 
     @property
     def end(self) -> Point:
-        pnt = self._occ_curve.EndPoint()
+        pnt = self.occ_curve.EndPoint()
         return Point(* pnt.XYZ())
 
     @property
     def is_closed(self) -> bool:
-        return self._occ_curve.IsClosed()
+        return self.occ_curve.IsClosed()
 
     @property
     def is_periodic(self) -> bool:
-        return self._occ_curve.IsPeriodic()
+        return self.occ_curve.IsPeriodic()
 
     @property
     def is_rational(self) -> bool:
-        return self._occ_curve.IsRational()
+        return self.occ_curve.IsRational()
 
     def copy(self):
         return BSplineCurve(self.poles,
@@ -124,9 +116,9 @@ class BSplineCurve:
                             self.is_periodic)
 
     def transform(self, T: Transformation) -> None:
-        _T = gp_Trsf()
-        _T.SetValues(* T.list)
-        self._occ_curve.Transform(T)
+        occ_T = gp_Trsf()
+        occ_T.SetValues(* T.list)
+        self.occ_curve.Transform(occ_T)
 
     def transformed(self, T: Transformation) -> BSplineCurve:
         copy = self.copy()
--- src/compas_occ/geometry/surfaces/bspline.py
+++ src/compas_occ/geometry/surfaces/bspline.py
@@ -9,21 +9,43 @@ from compas_occ.interop.arrays import array_of_points, array_of_floats, array_of
 
 from OCC.Core.gp import gp_Trsf
 from OCC.Core.Geom import Geom_BSplineSurface
+from OCC.Core.TColgp import TColgp_Array2OfPnt
+from OCC.Core.TColStd import TColStd_Array1OfReal, TColStd_Array1OfInteger
+from OCC.Core.STEPControl import STEPControl_Writer, STEPControl_AsIs
+from OCC.Core.Interface import Interface_Static_SetCVal
+from OCC.Core.IFSelect import IFSelect_RetDone
+from OCC.Core.TopoDS import topods_Face, TopoDS_Shape, TopoDS_Face
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeFace
+from OCC.Core.GeomFill import GeomFill_BSplineCurves, GeomFill_CoonsStyle
 
 
 class BSplineSurface:
 
-    def __init__(self,
-                 poles: List[Point],
-                 u_knots: List[float],
-                 v_knots: List[float],
-                 u_mults: List[int],
-                 v_mults: List[int],
-                 u_degree: int,
-                 v_degree: int,
-                 is_periodic_u: bool = False,
-                 is_periodic_v: bool = False):
-        self._occ_surface = Geom_BSplineSurface(
+    def __init__(self):
+        self.occ_surface = None
+
+    def __eq__(self, other: BSplineSurface) -> bool:
+        return self.occ_surface.IsEqual(other.occ_surface)
+
+    @classmethod
+    def from_occ(cls, occ_surface) -> BSplineSurface:
+        surface = cls()
+        surface.occ_surface = occ_surface
+        return surface
+
+    @classmethod
+    def from_parameters(cls,
+                        poles: List[Point],
+                        u_knots: List[float],
+                        v_knots: List[float],
+                        u_mults: List[int],
+                        v_mults: List[int],
+                        u_degree: int,
+                        v_degree: int,
+                        is_u_periodic: bool = False,
+                        is_v_periodic: bool = False) -> BSplineSurface:
+        surface = cls()
+        surface.occ_surface = Geom_BSplineSurface(
             array_of_points(poles),
             array_of_floats(u_knots),
             array_of_floats(v_knots),
@@ -31,33 +53,111 @@ class BSplineSurface:
             array_of_integers(v_mults),
             u_degree,
             v_degree,
-            is_periodic_u,
-            is_periodic_v
+            is_u_periodic,
+            is_v_periodic
         )
+        return surface
 
     @classmethod
-    def from_step(cls, filepath):
+    def from_step(cls, filepath) -> BSplineSurface:
         pass
 
-    def to_step(self, filepath):
-        pass
+    def to_step(self, filepath, schema="AP203") -> None:
+        step_writer = STEPControl_Writer()
+        Interface_Static_SetCVal("write.step.schema", schema)
+        step_writer.Transfer(self.occ_face, STEPControl_AsIs)
+        status = step_writer.Write(filepath)
+        if status != IFSelect_RetDone:
+            raise AssertionError("Operation failed.")
 
-    def __eq__(self, other: BSplineSurface) -> bool:
-        return self._occ_surface.IsEqual(other._occ_surface)
+    @classmethod
+    def from_fill(cls, curve1, curve2) -> BSplineSurface:
+        surface = cls()
+        occ_fill = GeomFill_BSplineCurves(curve1.occ_curve, curve2.occ_curve, GeomFill_CoonsStyle)
+        surface.occ_surface = occ_fill.Surface()
+        return surface
 
-    # def copy(self):
-    #     return BSplineSurface(self.poles,
-    #                           self.knots,
-    #                           self.multiplicities,
-    #                           self.degree,
-    #                           self.is_periodic)
+    @property
+    def occ_shape(self) -> TopoDS_Shape:
+        return BRepBuilderAPI_MakeFace(self.occ_surface, 1e-6).Shape()
 
-    # def transform(self, T: Transformation) -> None:
-    #     _T = gp_Trsf()
-    #     _T.SetValues(* T.list)
-    #     self._occ_curve.Transform(T)
+    @property
+    def occ_face(self) -> TopoDS_Face:
+        return topods_Face(self.occ_shape)
 
-    # def transformed(self, T: Transformation) -> BSplineSurface:
-    #     copy = self.copy()
-    #     copy.transform(T)
-    #     return copy
+    @property
+    def occ_poles(self) -> TColgp_Array2OfPnt:
+        return self.occ_surface.Poles()
+
+    @property
+    def occ_u_knots(self) -> TColStd_Array1OfReal:
+        return self.occ_surface.UKnots()
+
+    @property
+    def occ_v_knots(self) -> TColStd_Array1OfReal:
+        return self.occ_surface.VKnots()
+
+    @property
+    def occ_u_mults(self) -> TColStd_Array1OfInteger:
+        return self.occ_surface.UMultiplicities()
+
+    @property
+    def occ_v_mults(self) -> TColStd_Array1OfInteger:
+        return self.occ_surface.VMultiplicities()
+
+    @property
+    def poles(self) -> List[List[Point]]:
+        return [Point(pole.X(), pole.Y(), pole.Z()) for pole in self.occ_poles]
+
+    @property
+    def u_knots(self) -> List[float]:
+        return self.occ_u_knots
+
+    @property
+    def v_knots(self) -> List[float]:
+        return self.occ_v_knots
+
+    @property
+    def u_mults(self) -> List[int]:
+        return self.occ_u_mults
+
+    @property
+    def v_mults(self) -> List[int]:
+        return self.occ_v_mults
+
+    @property
+    def u_degree(self) -> int:
+        return self.occ_surface.UDegree()
+
+    @property
+    def v_degree(self) -> int:
+        return self.occ_surface.VDegree()
+
+    @property
+    def is_u_periodic(self) -> bool:
+        return self.occ_surface.IsUPeriodic()
+
+    @property
+    def is_v_periodic(self) -> bool:
+        return self.occ_surface.IsVPeriodic()
+
+    def copy(self) -> BSplineSurface:
+        return BSplineSurface(self.poles,
+                              self.u_knots,
+                              self.v_knots,
+                              self.u_mults,
+                              self.v_mults,
+                              self.u_degree,
+                              self.v_degree,
+                              self.is_u_periodic,
+                              self.is_v_periodic)
+
+    def transform(self, T: Transformation) -> None:
+        _T = gp_Trsf()
+        _T.SetValues(* T.list)
+        self.occ_surface.Transform(T)
+
+    def transformed(self, T: Transformation) -> BSplineSurface:
+        copy = self.copy()
+        copy.transform(T)
+        return copy
