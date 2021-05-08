89f3be870cb605a12bf50e427c385ef13db2dae8
brgcode <vanmelet@ethz.ch>
2021-05-08 09:12:36 +0200

    monkey primitives?

---
+++ src/compas_occ/geometry/primitives.py
@@ -0,0 +1,34 @@
+from compas.geometry import (
+    Point,
+    Vector,
+    Line
+)
+
+from OCC.Core.gp import (
+    gp_Pnt,
+    gp_Vec,
+    gp_Dir,
+    gp_Lin
+)
+
+
+def compas_point_to_occ_point(self: Point) -> gp_Pnt:
+    return gp_Pnt(* self)
+
+
+def compas_vector_to_occ_vector(self: Vector) -> gp_Vec:
+    return gp_Vec(* self)
+
+
+def compas_vector_to_occ_direction(self: Vector) -> gp_Dir:
+    return gp_Dir(* self)
+
+
+def compas_line_to_occ_line(self: Line) -> gp_Lin:
+    return gp_Lin(self.start.to_occ_point(), self.direction.to_occ_direction())
+
+
+Point.to_occ_point = compas_point_to_occ_point
+Vector.to_occ_vector = compas_vector_to_occ_vector
+Vector.to_occ_direction = compas_vector_to_occ_direction
+Line.to_occ_line = compas_line_to_occ_line
