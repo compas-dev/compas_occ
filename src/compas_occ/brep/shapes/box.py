27afb07ebae4e091daffedad9c6b9481153a8304
brgcode <vanmelet@ethz.ch>
2021-05-09 20:10:20 +0200

    box solid and shell

--- src/compas_occ/brep/shapes/box.py
+++ src/compas_occ/brep/shapes/box.py
@@ -1,27 +1,32 @@
 from compas.geometry import Box
+from compas.geometry import Translation
 
-from OCC.Core.BRep import BRep_Builder
-from OCC.Core.BRepBuilderAPI import (
-    BRepBuilderAPI_MakePolygon,
-    BRepBuilderAPI_MakeFace
+from OCC.Core.TopoDS import (
+    TopoDS_Solid,
+    TopoDS_Shell
 )
-from OCC.Core.TopoDS import TopoDS_Shell
-from OCC.Core.gp import gp_Pnt
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeBox
+from OCC.Core.gp import (
+    gp_Pnt,
+    gp_Dir,
+    gp_Ax2
+)
 
 
 class Box(Box):
 
-    def to_occ(self):
-        """Convert a COMPAS box to an OCC BRep."""
-        shell = TopoDS_Shell()
-        builder = BRep_Builder()
-        builder.MakeShell(shell)
-        vertices = [gp_Pnt(*xyz) for xyz in self.vertices]
-        for face in self.faces:
-            polygon = BRepBuilderAPI_MakePolygon()
-            for vertex in face:
-                polygon.Add(vertices[vertex])
-            wire = polygon.Wire()
-            shape = BRepBuilderAPI_MakeFace(wire).Shape()
-            builder.Add(shell, shape)
-        return shell
+    def _to_occ(self) -> BRepBuilderAPI_MakeBox:
+        xaxis = self.frame.xaxis.scaled(-self.xsize)
+        yaxis = self.frame.yaxis.scaled(-self.ysize)
+        zaxis = self.frame.zaxis.scaled(-self.zsize)
+        frame = self.frame.transformed(Translation.from_vector(xaxis + yaxis + zaxis))
+        ax2 = gp_Ax2(gp_Pnt(* frame.point), gp_Dir(* frame.zaxis), gp_Dir(* frame.xaxis))
+        return BRepBuilderAPI_MakeBox(ax2, self.xsize, self.ysize, self.zsize)
+
+    def to_occ_solid(self) -> TopoDS_Solid:
+        """Convert a COMPAS box shape to an OCC solid."""
+        return self._to_occ.Solid()
+
+    def to_occ_shell(self) -> TopoDS_Shell:
+        """Convert a COMPAS box shape to an OCC shell."""
+        return self._to_occ.Shell()
