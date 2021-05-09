5cfbd4400b0d8ef9d20ebc04447889c9bd0358ad
brgcode <vanmelet@ethz.ch>
2021-05-09 20:48:16 +0200

    added box and moved box and sphere to geometry primitives

---
+++ src/compas_occ/geometry/shapes.py
@@ -0,0 +1,52 @@
+from compas.geometry import (
+    Box,
+    Sphere,
+    Translation
+)
+
+from OCC.Core.TopoDS import (
+    TopoDS_Solid,
+    TopoDS_Shell
+)
+from OCC.Core.BRepPrimAPI import (
+    BRepPrimAPI_MakeBox,
+    BRepPrimAPI_MakeSphere
+)
+from OCC.Core.gp import (
+    gp_Pnt,
+    gp_Dir,
+    gp_Ax2
+)
+
+
+class Box(Box):
+
+    def _to_occ(self) -> BRepPrimAPI_MakeBox:
+        xaxis = self.frame.xaxis.scaled(-self.xsize)
+        yaxis = self.frame.yaxis.scaled(-self.ysize)
+        zaxis = self.frame.zaxis.scaled(-self.zsize)
+        frame = self.frame.transformed(Translation.from_vector(xaxis + yaxis + zaxis))
+        ax2 = gp_Ax2(gp_Pnt(* frame.point), gp_Dir(* frame.zaxis), gp_Dir(* frame.xaxis))
+        return BRepPrimAPI_MakeBox(ax2, self.xsize, self.ysize, self.zsize)
+
+    def to_occ_solid(self) -> TopoDS_Solid:
+        """Convert a COMPAS box to an OCC solid."""
+        return self._to_occ.Solid()
+
+    def to_occ_shell(self) -> TopoDS_Shell:
+        """Convert a COMPAS box to an OCC shell."""
+        return self._to_occ.Shell()
+
+
+class Sphere(Sphere):
+
+    def _to_occ(self) -> BRepPrimAPI_MakeSphere:
+        return BRepPrimAPI_MakeSphere(gp_Pnt(* self.point), self.radius)
+
+    def _to_occ_solid(self) -> TopoDS_Solid:
+        """Convert a COMPAS sphere to an OCC solid."""
+        return self._to_occ.Solid()
+
+    def _to_occ_shell(self) -> TopoDS_Shell:
+        """Convert a COMPAS sphere to an OCC shell."""
+        return self._to_occ.Shell()
