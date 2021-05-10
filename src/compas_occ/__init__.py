d47338be61aab95924a916d8055968f5421e36e9
brgcode <vanmelet@ethz.ch>
2021-05-10 11:36:08 +0200

    update docs

--- src/compas_occ/__init__.py
+++ src/compas_occ/__init__.py
@@ -9,6 +9,9 @@ compas_occ
 .. toctree::
     :maxdepth: 1
 
+    compas_occ.brep
+    compas_occ.geometry
+    compas_occ.interop
 
 """
 
--- src/compas_occ/brep/__init__.py
+++ src/compas_occ/brep/__init__.py
@@ -6,4 +6,35 @@ compas_occ.brep
 .. currentmodule:: compas_occ.brep
 
 
+Datastructures
+==============
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    datastructures.BRepShape
+
+
+Primitives
+==========
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    primitives.Box
+    primitives.Sphere
+
+
+Booleans
+========
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    booleans.boolean_union_shape_shape
+
+
 """
