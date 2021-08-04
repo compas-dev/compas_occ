a519ee701a4c387d596f6f1044f5028aec93ec53
brgcode <vanmelet@ethz.ch>
2021-08-04 17:06:40 +0200

    docs

--- src/compas_occ/geometry/__init__.py
+++ src/compas_occ/geometry/__init__.py
@@ -5,23 +5,28 @@ compas_occ.geometry
 
 .. currentmodule:: compas_occ.geometry
 
-Classes
-=======
+Curves
+======
 
 .. autosummary::
     :toctree: generated/
     :nosignatures:
 
+    Curve
     BSplineCurve
+
+Surfaces
+========
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    Surface
     BSplineSurface
 
-Functions
-=========
-
-.. autosummary::
-    :toctree: generated/
-    :nosignatures:
-
 """
+from .curves import Curve  # noqa: F401
 from .curves import BSplineCurve  # noqa: F401
+from .surfaces import Surface  # noqa: F401
 from .surfaces import BSplineSurface  # noqa: F401
