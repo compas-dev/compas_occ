5804db379330f1b04a1866a7ff00a569ecf4290e
brgcode <vanmelet@ethz.ch>
2021-08-04 09:57:42 +0200

    rhino-style curve classes

--- src/compas_occ/geometry/curves/__init__.py
+++ src/compas_occ/geometry/curves/__init__.py
@@ -1,1 +1,2 @@
+from ._curve import Curve  # noqa F401
 from .bspline import BSplineCurve  # noqa F401
--- src/compas_occ/geometry/curves/bspline.py
+++ src/compas_occ/geometry/curves/bspline.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
-from typing import List
+from typing import Dict, List
 from compas.geometry import Point
 from compas.geometry import Transformation
 from compas.utilities import linspace
 
 from compas_occ.interop.arrays import (
+    harray1_from_points1,
     array1_from_points1,
     array1_from_floats1,
     array1_from_integers1,
     points1_from_array1
 )
+
+from ._curve import Curve
 
 from OCC.Core.gp import gp_Trsf
 from OCC.Core.Geom import Geom_BSplineCurve
@@ -38,14 +41,104 @@ from OCC.Core.STEPControl import (
 )
 
 
-class BSplineCurve:
+class BSplineCurve(Curve):
     """Wrapper for OCC BSplineCurve."""
 
-    def __init__(self) -> BSplineCurve:
+    @property
+    def DATASCHEMA(self):
+        from schema import Schema
+        from compas.data import is_float3
+        from compas.data import is_sequence_of_int
+        from compas.data import is_sequence_of_float
+        return Schema({
+            'points': lambda points: all(is_float3(point) for point in points),
+            'knots': is_sequence_of_float,
+            'multiplicities': is_sequence_of_int,
+            'degree': int,
+            'is_periodic': bool
+        })
+
+    @property
+    def JSONSCHEMANAME(self):
+        raise NotImplementedError
+
+    def __init__(self, name=None) -> BSplineCurve:
+        super().__init__(name=name)
         self.occ_curve = None
 
     def __eq__(self, other: BSplineCurve) -> bool:
         return self.occ_curve.IsEqual(other.occ_curve)
+
+    def __str__(self):
+        lines = [
+            'BSplineCurve',
+            '------------',
+            f'Poles: {self.poles}',
+            f'Knots: {self.knots}',
+            f'Mults: {self.multiplicities}',
+            f'Degree: {self.degree}',
+            f'Order: {self.order}',
+            f'Domain: {self.domain}',
+            f'Closed: {self.is_closed}',
+            f'Periodic: {self.is_periodic}',
+            f'Rational: {self.is_rational}',
+        ]
+        return "\n".join(lines)
+
+    # ==============================================================================
+    # Data
+    # ==============================================================================
+
+    @property
+    def data(self) -> Dict:
+        return {
+            'points': [point.data for point in self.points],
+            'knots': self.knots,
+            'multiplicities': self.multiplicities,
+            'degree': self.degree,
+            'is_periodic': self.is_periodic
+        }
+
+    @data.setter
+    def data(self, data: Dict):
+        poles = [Point.from_data(point) for point in data['points']]
+        knots = data['knots']
+        multiplicities = data['multiplicities']
+        degree = data['degree']
+        is_periodic = data['is_periodic']
+        self.occ_curve = Geom_BSplineCurve(
+            array1_from_points1(poles),
+            array1_from_floats1(knots),
+            array1_from_integers1(multiplicities),
+            degree,
+            is_periodic,
+        )
+
+    @classmethod
+    def from_data(cls, data: Dict) -> BSplineCurve:
+        """Construct a BSpline curve from its data representation.
+
+        Parameters
+        ----------
+        data : dict
+            The data dictionary.
+
+        Returns
+        -------
+        :class:`compas.geometry.BezierCurve`
+            The constructed bezier curve.
+
+        """
+        poles = [Point.from_data(point) for point in data['points']]
+        knots = data['knots']
+        multiplicities = data['multiplicities']
+        degree = data['degree']
+        is_periodic = data['is_periodic']
+        return BSplineCurve.from_parameters(poles, knots, multiplicities, degree, is_periodic)
+
+    # ==============================================================================
+    # Constructors
+    # ==============================================================================
 
     @classmethod
     def from_occ(cls, occ_curve: Geom_BSplineCurve) -> BSplineCurve:
@@ -60,6 +153,7 @@ class BSplineCurve:
                         multiplicities: List[int],
                         degree: int,
                         is_periodic: bool = False) -> BSplineCurve:
+        """Construct a curve from poles and knots."""
         curve = cls()
         curve.occ_curve = Geom_BSplineCurve(
             array1_from_points1(poles),
@@ -73,7 +167,9 @@ class BSplineCurve:
     @classmethod
     def from_interpolation(cls, points: List[Point]) -> BSplineCurve:
         curve = cls()
-        curve.occ_curve = GeomAPI_Interpolate(array1_from_points1(points)).Curve()
+        interp = GeomAPI_Interpolate(harray1_from_points1(points), False, 1e-3)
+        interp.Perform()
+        curve.occ_curve = interp.Curve()
         return curve
 
     @classmethod
@@ -93,6 +189,26 @@ class BSplineCurve:
             return
         return cls.from_occ(res[0])
 
+    @classmethod
+    def from_arc(cls, arc, degree, pointcount=None):
+        pass
+
+    @classmethod
+    def from_circle(cls, circle, degree, pointcount=None):
+        pass
+
+    @classmethod
+    def from_ellipse(cls, ellipse, degree, pointcount=None):
+        pass
+
+    @classmethod
+    def from_line(cls, line, degree, pointcount=None):
+        pass
+
+    # ==============================================================================
+    # Conversions
+    # ==============================================================================
+
     def to_step(self, filepath: str, schema: str = "AP203") -> None:
         step_writer = STEPControl_Writer()
         Interface_Static_SetCVal("write.step.schema", schema)
@@ -101,8 +217,9 @@ class BSplineCurve:
         if status != IFSelect_RetDone:
             raise AssertionError("Operation failed.")
 
-    def to_locus(self, resolution=100):
-        return self.xyz(resolution)
+    # ==============================================================================
+    # OCC
+    # ==============================================================================
 
     @property
     def occ_shape(self) -> TopoDS_Shape:
@@ -116,39 +233,67 @@ class BSplineCurve:
     def occ_poles(self) -> TColgp_Array1OfPnt:
         return self.occ_curve.Poles()
 
+    occ_points = occ_poles
+
     @property
     def occ_knots(self) -> TColStd_Array1OfReal:
         return self.occ_curve.Knots()
 
     @property
+    def occ_knotsequence(self) -> TColStd_Array1OfReal:
+        return self.occ_curve.KnotSequence()
+
+    @property
     def occ_multiplicities(self) -> TColStd_Array1OfInteger:
         return self.occ_curve.Multiplicities()
+
+    # ==============================================================================
+    # Properties
+    # ==============================================================================
 
     @property
     def poles(self) -> List[Point]:
         return points1_from_array1(self.occ_poles)
 
+    points = poles
+
     @property
     def knots(self) -> List[float]:
-        return self.occ_knots
+        return list(self.occ_knots)
+
+    @property
+    def knotsequence(self) -> List[float]:
+        return list(self.occ_knotsequence)
 
     @property
     def multiplicities(self) -> List[int]:
-        return self.occ_multiplicities
+        return list(self.occ_multiplicities)
 
     @property
     def degree(self) -> int:
         return self.occ_curve.Degree()
 
     @property
+    def dimension(self):
+        pass
+
+    @property
+    def domain(self):
+        return self.occ_curve.FirstParameter(), self.occ_curve.LastParameter()
+
+    @property
+    def order(self):
+        return self.degree + 1
+
+    @property
     def start(self) -> Point:
         pnt = self.occ_curve.StartPoint()
-        return Point(* pnt.XYZ())
+        return Point(pnt.X(), pnt.Y(), pnt.Z())
 
     @property
     def end(self) -> Point:
         pnt = self.occ_curve.EndPoint()
-        return Point(* pnt.XYZ())
+        return Point(pnt.X(), pnt.Y(), pnt.Z())
 
     @property
     def is_closed(self) -> bool:
@@ -161,6 +306,18 @@ class BSplineCurve:
     @property
     def is_rational(self) -> bool:
         return self.occ_curve.IsRational()
+
+    @property
+    def bounding_box(self):
+        pass
+
+    @property
+    def length(self):
+        pass
+
+    # ==============================================================================
+    # Methods
+    # ==============================================================================
 
     def copy(self) -> BSplineCurve:
         return BSplineCurve(self.poles,
@@ -179,16 +336,101 @@ class BSplineCurve:
         copy.transform(T)
         return copy
 
-    def domain(self):
-        return self.occ_curve.FirstParameter(), self.occ_curve.LastParameter()
-
     def space(self, n: int = 10) -> List[float]:
-        u, v = self.domain()
+        u, v = self.domain
         return linspace(u, v, n)
-
-    def point_at(self, u: float) -> Point:
-        point = self.occ_curve.Value(u)
-        return Point(point.X(), point.Y(), point.Z())
 
     def xyz(self, n: int = 10) -> List[Point]:
         return [self.point_at(param) for param in self.space(n)]
+
+    def locus(self, resolution=100):
+        """Compute the locus of all points on the curve.
+
+        Parameters
+        ----------
+        resolution : int
+            The number of intervals at which a point on the
+            curve should be computed. Defaults to 100.
+
+        Returns
+        -------
+        list
+            Points along the curve.
+        """
+        return self.xyz(resolution)
+
+    def point_at(self, u: float) -> Point:
+        """Compute a point on the curve.
+
+        Parameters
+        ----------
+        t : float
+            The value of the curve parameter. Must be between 0 and 1.
+
+        Returns
+        -------
+        Point
+            the corresponding point on the curve.
+        """
+        point = self.occ_curve.Value(u)
+        return Point(point.X(), point.Y(), point.Z())
+
+    def tangent_at(self, t):
+        """Compute the tangent vector at a point on the curve.
+
+        Parameters
+        ----------
+        t : float
+            The value of the curve parameter. Must be between 0 and 1.
+
+        Returns
+        -------
+        Vector
+            The corresponding tangent vector.
+
+        """
+        pass
+
+    def curvature_at(self, t):
+        """Compute the curvature at a point on the curve.
+
+        Parameters
+        ----------
+        t : float
+            The value of the curve parameter. Must be between 0 and 1.
+
+        Returns
+        -------
+        Vector
+            The corresponding curvature vector.
+
+        """
+        pass
+
+    def frame_at(self, t):
+        """Compute the local frame at a point on the curve.
+
+        Parameters
+        ----------
+        t : float
+            The value of the curve parameter. Must be between 0 and 1.
+
+        Returns
+        -------
+        Frame
+            The corresponding local frame.
+
+        """
+        pass
+
+    def closest_point(self, point, distance=None):
+        pass
+
+    def divide_by_count(self, count):
+        pass
+
+    def divide_by_length(self, length):
+        pass
+
+    def fair(self):
+        pass
