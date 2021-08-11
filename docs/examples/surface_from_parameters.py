7fe88c230be9b5ff9e8a638a671d7be9451b6f10
brgcode <vanmelet@ethz.ch>
2021-08-11 17:33:25 +0200

    asymmetrical

--- docs/examples/surface_from_parameters.py
+++ docs/examples/surface_from_parameters.py
@@ -10,22 +10,24 @@ points = [
     [Point(0, 1, 0), Point(1, 1, 2), Point(2, 1, 2), Point(3, 1, 0)],
     [Point(0, 2, 0), Point(1, 2, 2), Point(2, 2, 2), Point(3, 2, 0)],
     [Point(0, 3, 0), Point(1, 3, 0), Point(2, 3, 0), Point(3, 3, 0)],
+    [Point(0, 4, 0), Point(1, 4, 0), Point(2, 4, 0), Point(3, 4, 0)],
 ]
 
 weights = [
     [1.0, 1.0, 1.0, 1.0],
     [1.0, 2.0, 2.0, 1.0],
     [1.0, 2.0, 2.0, 1.0],
-    [1.0, 1.0, 1.0, 1.0]
+    [1.0, 1.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0, 1.0],
 ]
 
 surface = NurbsSurface.from_parameters(
     points=points,
     weights=weights,
     u_knots=[0.0, 1.0],
-    v_knots=[0.0, 1.0],
+    v_knots=[0.0, 1.0, 2.0],
     u_mults=[4, 4],
-    v_mults=[4, 4],
+    v_mults=[4, 1, 4],
     u_degree=3,
     v_degree=3,
 )
