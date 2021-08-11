6bbc7da7ff1658a6ee86cfd97053f2c80720539b
brgcode <vanmelet@ethz.ch>
2021-08-11 09:48:31 +0200

    spacepoints example

---
+++ docs/examples/surface_spacepoints.py
@@ -0,0 +1,43 @@
+from compas.geometry import Point, Polyline
+from compas_occ.geometry import NurbsSurface
+
+from compas_view2.app import App
+from compas_view2.objects import Collection
+
+points = [
+    [Point(0, 0, 0), Point(1, 0, 0), Point(2, 0, 0), Point(3, 0, 0)],
+    [Point(0, 1, 0), Point(1, 1, 2), Point(2, 1, 2), Point(3, 1, 0)],
+    [Point(0, 2, 0), Point(1, 2, 2), Point(2, 2, 2), Point(3, 2, 0)],
+    [Point(0, 3, 0), Point(1, 3, 0), Point(2, 3, 0), Point(3, 3, 0)],
+]
+
+surface = NurbsSurface.from_points(points=points)
+
+print(surface)
+
+# ==============================================================================
+# Points over UV space
+# ==============================================================================
+
+spacepoints = surface.xyz(nu=50, nv=10)
+
+# ==============================================================================
+# Visualisation
+# ==============================================================================
+
+view = App()
+
+for row in surface.points:
+    view.add(Collection(row), size=20, color=(1, 0, 0))
+
+for row in surface.points:
+    view.add(Polyline(row), linewidth=2, linecolor=(0.3, 0.3, 0.3))
+
+for col in zip(* surface.points):
+    view.add(Polyline(col), linewidth=2, linecolor=(0.3, 0.3, 0.3))
+
+view.add(surface.to_mesh(u=50), show_edges=False)
+
+view.add(Collection(spacepoints))
+
+view.run()
---
+++ docs/examples/surface_spacepoints.rst
@@ -0,0 +1,10 @@
+********************************************************************************
+Surface Points Over UV Space
+********************************************************************************
+
+.. figure:: /_images/example_surface_spacepoints.png
+    :figclass: figure
+    :class: figure-img img-fluid
+
+.. literalinclude:: surface_spacepoints.py
+    :language: python
