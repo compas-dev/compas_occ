623f7ad04052c1f149bd72a4256856a89311a592
brgcode <vanmelet@ethz.ch>
2021-08-13 10:00:33 +0200

    isocurve examples

---
+++ docs/examples/surface_isocurves.py
@@ -0,0 +1,49 @@
+from compas.geometry import Point, Polyline
+from compas_occ.geometry import NurbsSurface
+
+from compas_view2.app import App
+
+points = [
+    [Point(0, 0, 0), Point(1, 0, 0), Point(2, 0, 0), Point(3, 0, 0), Point(4, 0, 0)],
+    [Point(0, 1, 0), Point(1, 1, 2), Point(2, 1, 2), Point(3, 1, 0), Point(4, 1, 0)],
+    [Point(0, 2, 0), Point(1, 2, 2), Point(2, 2, 2), Point(3, 2, 0), Point(4, 2, 0)],
+    [Point(0, 3, 0), Point(1, 3, 0), Point(2, 3, 0), Point(3, 3, 0), Point(4, 3, 0)],
+]
+
+surface = NurbsSurface.from_points(points=points)
+
+# print(surface)
+
+# ==============================================================================
+# Isocurves
+# ==============================================================================
+
+u_curves = []
+for u in surface.u_space():
+    u_curves.append(surface.u_isocurve(u))
+
+v_curves = []
+for v in surface.v_space():
+    v_curves.append(surface.v_isocurve(v))
+
+# ==============================================================================
+# Visualisation
+# ==============================================================================
+
+view = App()
+
+# for row in surface.points:
+#     view.add(Polyline(row), show_points=True, pointsize=20, pointcolor=(1, 0, 0), linewidth=2, linecolor=(1.0, 0, 0))
+
+# for col in zip(* surface.points):
+#     view.add(Polyline(col), linewidth=2, linecolor=(0, 1.0, 0))
+
+# view.add(surface.to_mesh(u=50), show_edges=False)
+
+for curve in u_curves:
+    view.add(Polyline(curve.locus()), linecolor=(1, 0, 0), linewidth=2)
+
+for curve in v_curves:
+    view.add(Polyline(curve.locus()), linecolor=(0, 1, 0), linewidth=2)
+
+view.run()
---
+++ docs/examples/surface_isocurves.rst
@@ -0,0 +1,10 @@
+********************************************************************************
+Surface Isocurves
+********************************************************************************
+
+.. figure:: /_images/example_surface_isocurves.png
+    :figclass: figure
+    :class: figure-img img-fluid
+
+.. literalinclude:: surface_isocurves.py
+    :language: python
