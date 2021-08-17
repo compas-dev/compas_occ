370d9aec773c6186297e5dd5697f5a364270efca
brgcode <vanmelet@ethz.ch>
2021-08-17 22:21:20 +0200

    example with view object

---
+++ docs/examples/surface_view2.py
@@ -0,0 +1,23 @@
+from compas.geometry import Point
+from compas_occ.geometry import NurbsSurface
+
+from compas_view2.app import App
+
+points = [
+    [Point(0, 0, 0), Point(1, 0, 0), Point(2, 0, 0), Point(3, 0, 0)],
+    [Point(0, 1, 0), Point(1, 1, 2), Point(2, 1, 2), Point(3, 1, 0)],
+    [Point(0, 2, 0), Point(1, 2, 2), Point(2, 2, 2), Point(3, 2, 0)],
+    [Point(0, 3, 0), Point(1, 3, 0), Point(2, 3, 0), Point(3, 3, 0)],
+]
+
+surface = NurbsSurface.from_points(points=points)
+
+# ==============================================================================
+# Visualisation
+# ==============================================================================
+
+view = App()
+
+view.add(surface)
+
+view.run()
