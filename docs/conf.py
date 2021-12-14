92abc87aa009b15cbc628b807672bbf1179b7d43
brgcode <van.mele@arch.ethz.ch>
2021-12-14 21:05:26 +0100

    add mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,19 @@ extensions = [
 
 # autodoc options
 
+autodoc_mock_imports = [
+    "System",
+    "clr",
+    "Eto",
+    "Rhino",
+    "Grasshopper",
+    "scriptcontext",
+    "rhinoscriptsyntax",
+    "bpy",
+    "bmesh",
+    "mathutils"
+]
+
 autodoc_default_options = {
     "undoc-members": True,
     "show-inheritance": True,
