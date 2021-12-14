fc6f78325b17b8f52aac36760b1921331ebb1e48
brgcode <van.mele@arch.ethz.ch>
2021-12-14 21:11:57 +0100

    Bump version to 0.3.2

--- src/compas_occ/__init__.py
+++ src/compas_occ/__init__.py
@@ -24,7 +24,7 @@ __author__ = ["tom van mele"]
 __copyright__ = "Block Research Group - ETH Zurich"
 __license__ = "MIT License"
 __email__ = "van.mele@arch.ethz.ch"
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 
 HERE = os.path.dirname(__file__)
