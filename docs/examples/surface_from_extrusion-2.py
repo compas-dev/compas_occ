45da70dd6f4662ff779bb1c70b67f72ee56c83f3
brgcode <van.mele@arch.ethz.ch>
2022-04-28 19:26:59 +0200

    extrusion examples

---
+++ docs/examples/surface_from_extrusion-2.py
@@ -0,0 +1,19 @@
+from compas.geometry import Point
+from compas.geometry import Vector
+from compas.geometry import NurbsCurve
+from compas_occ.geometry import OCCNurbsSurface
+from compas_view2.app import App
+
+points = [Point(0, 0, 0), Point(0, -6, 3), Point(0, 2, 6), Point(0, -2, 9)]
+curve = NurbsCurve.from_points(points)
+vector = Vector(5, 0, 0)
+
+surface = OCCNurbsSurface.from_extrusion(curve, vector)
+
+viewer = App()
+viewer.view.camera.position = [-7, -10, 6]
+viewer.view.camera.target = [2, 0, 5]
+
+viewer.add(curve.to_polyline(), linewidth=5, color=(1, 0, 0))
+viewer.add(surface.to_mesh())
+viewer.show()
