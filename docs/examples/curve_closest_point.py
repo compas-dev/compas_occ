a289247fda7a2d5732563a726f8d118bbff5d0a6
brgcode <van.mele@arch.ethz.ch>
2022-04-30 09:19:22 +0200

    sty;e

--- docs/examples/curve_closest_point.py
+++ docs/examples/curve_closest_point.py
@@ -7,20 +7,15 @@ from compas_view2.app import App
 points = [Point(0, 0, 0), Point(3, 0, 2), Point(6, 0, -3), Point(8, 0, 0)]
 curve = OCCNurbsCurve.from_interpolation(points)
 
-projection_point = Point(2, -1, 0)
+point = Point(2, -1, 0)
+closest, t = curve.closest_point(point, return_parameter=True)
 
-closest_point, t = curve.closest_point(projection_point, return_parameter=True)
-
-print(curve.point_at(t) == closest_point)
-
-# ==============================================================================
-# Visualisation
-# ==============================================================================
+print(curve.point_at(t) == closest)
 
 view = App()
 
 view.add(Polyline(curve.locus()), linewidth=3)
-view.add(projection_point, pointcolor=(0, 0, 1))
-view.add(closest_point, pointcolor=(1, 0, 0))
+view.add(point, pointcolor=(0, 0, 1))
+view.add(closest, pointcolor=(1, 0, 0))
 
 view.run()
