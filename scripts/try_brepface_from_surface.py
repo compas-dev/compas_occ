55f0d0e1454b3b857bf06caf5d844a7cf7c6df43
brgcode <van.mele@arch.ethz.ch>
2022-05-03 08:58:32 +0200

    test scripts

--- scripts/try_brepface_from_surface.py
+++ scripts/try_brepface_from_surface.py
@@ -2,17 +2,13 @@ from compas.geometry import Point
 from compas.geometry import Circle
 from compas.geometry import Plane
 from compas.geometry import Vector
-from compas_occ.brep.brepedge import BRepEdge
+from compas_occ.brep import BRepEdge
 from compas_occ.geometry import OCCNurbsSurface
 from compas_occ.geometry import OCCNurbsCurve
-from compas_occ.geometry import OCCCurve
 from compas_occ.brep import BRepLoop
 from compas_occ.brep import BRepFace
 from compas_occ.brep import BRep
 from compas_view2.app import App
-
-from OCC.Core.GeomProjLib import geomprojlib_Project
-from OCC.Core.GeomProjLib import geomprojlib_Curve2d
 
 points = [
     [Point(0, 0, 0), Point(1, 0, 0), Point(2, 0, 0), Point(3, 0, 0)],
@@ -23,35 +19,27 @@ points = [
 
 surface = OCCNurbsSurface.from_points(points=points)
 
-curve = OCCNurbsCurve.from_circle(
-    Circle(Plane(Point(1.5, 1.5, 1.5), Vector(0, 0, 1)), 0.5)
-)
+circle = Circle(Plane(Point(1.5, 1.5, 1.5), Vector(0, 0, 1)), 0.5)
+curve = OCCNurbsCurve.from_circle(circle)
+curve = curve.projected(surface)
+curve = curve.embedded(surface)
 
-result = geomprojlib_Project(curve.occ_curve, surface.occ_surface)
-projection = OCCCurve.from_occ(result)
-
-result = geomprojlib_Curve2d(projection.occ_curve, surface.occ_surface)
-curve2d = OCCCurve.from_occ(result)
-
-edge = BRepEdge.from_curve(curve2d, surface)
+edge = BRepEdge.from_curve(curve, surface)
 loop = BRepLoop.from_edges([edge])
-face = BRepFace.from_surface(surface, loop=loop, inside=False)
+
+face = BRepFace.from_surface(surface)
+face.add_loop(loop)
+
 brep = BRep.from_faces([face])
-
-print(brep.shape)
-
 mesh = brep.to_tesselation()
-print(mesh)
-
-for edge in brep.edges:
-    print(edge)
 
 viewer = App()
-
-for mesh in brep.to_meshes():
-    viewer.add(mesh)
-# viewer.add(surface.to_mesh())
-viewer.add(curve.to_polyline())
-viewer.add(projection.to_polyline(), color=(1.0, 0, 0), linewidth=5)
-
+viewer.add(mesh, show_edges=False)
+for edge in brep.edges:
+    if edge.is_line:
+        viewer.add(edge.to_line(), linewidth=2)
+    elif edge.is_circle:
+        viewer.add(edge.curve.to_polyline(), linewidth=2)
+    elif edge.is_bspline:
+        viewer.add(edge.curve.to_polyline(), linewidth=2)
 viewer.show()
---
+++ scripts/try_brepface_with_hole.py
@@ -0,0 +1,34 @@
+from compas.geometry import Plane, Circle
+from compas_occ.brep import BRepEdge, BRepLoop, BRepFace
+from compas_occ.brep import BRep
+from compas_view2.app import App
+
+plane = Plane.worldXY()
+
+circle1 = Circle(Plane([2, 2, 0], [0, 0, 1]), 1.0)
+circle2 = Circle(Plane([-2, -2, 0], [0, 0, 1]), 2.0)
+circle3 = Circle(Plane([2, -2, 0], [0, 0, 1]), 0.5)
+
+loop1 = BRepLoop.from_edges([BRepEdge.from_circle(circle1)])
+loop2 = BRepLoop.from_edges([BRepEdge.from_circle(circle2)])
+loop3 = BRepLoop.from_edges([BRepEdge.from_circle(circle3)])
+
+face = BRepFace.from_plane(plane, udomain=(-5, 5), vdomain=(-5, 5))
+face.add_loops([loop1, loop2, loop3], reverse=True)
+
+for loop in face.loops:
+    print(loop.loop.Orientation())
+
+brep = BRep.from_faces([face])
+mesh = brep.to_tesselation()
+
+viewer = App()
+viewer.add(mesh, show_edges=False)
+
+for edge in brep.edges:
+    if edge.is_line:
+        viewer.add(edge.to_line(), linewidth=2)
+    elif edge.is_circle:
+        viewer.add(edge.curve.to_polyline(), linewidth=2)
+
+viewer.show()
