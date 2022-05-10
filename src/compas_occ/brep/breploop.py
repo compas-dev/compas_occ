5ddddbc593a480d3ada8775cbbaad71d00fac3f8
brgcode <van.mele@arch.ethz.ch>
2022-05-10 17:10:20 +0200

    rebase on data and mark props as occ

--- src/compas_occ/brep/breploop.py
+++ src/compas_occ/brep/breploop.py
@@ -7,6 +7,7 @@ from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeWire
 from OCC.Core.BRepAlgo import brepalgo_IsValid
 from OCC.Core.ShapeFix import ShapeFix_Wire
 
+from compas.data import Data
 from compas.utilities import pairwise
 from compas.geometry import Polyline
 from compas.geometry import Polygon
@@ -15,12 +16,12 @@ from compas_occ.brep import BRepVertex
 from compas_occ.brep import BRepEdge
 
 
-class BRepLoop:
+class BRepLoop(Data):
     """Class representing an edge loop in the BRep of a geometric shape.
 
     Parameters
     ----------
-    loop : ``TopoDS_Wire``
+    occ_wire : ``TopoDS_Wire``
         An OCC BRep wire.
 
     Attributes
@@ -32,27 +33,40 @@ class BRepLoop:
 
     Other Attributes
     ----------------
-    loop : ``TopoDS_Wire``
+    occ_wire : ``TopoDS_Wire``
         The OCC BRep wire.
 
     """
 
-    def __init__(self, loop: TopoDS_Wire):
-        self._loop = None
-        self.loop = loop
+    def __init__(self, occ_wire: TopoDS_Wire = None):
+        super().__init__()
+        self._occ_wire = None
+        if occ_wire:
+            self.occ_wire = occ_wire
+
+    # @property
+    # def data(self):
+    #     return {
+    #         "edges": [edge.data for edge in self.edges],
+    #     }
+
+    # @data.setter
+    # def data(self, data):
+    #     edges = [BRepEdge.from_data(edgedata) for edgedata in data["edges"]]
+    #     self.edges = edges
 
     @property
-    def loop(self):
-        return self._loop
+    def occ_wire(self):
+        return self._occ_wire
 
-    @loop.setter
-    def loop(self, loop: TopoDS_Wire):
-        self._loop = topods_Wire(loop)
+    @occ_wire.setter
+    def occ_wire(self, loop: TopoDS_Wire):
+        self._occ_wire = topods_Wire(loop)
 
     @property
     def vertices(self) -> List[BRepVertex]:
         vertices = []
-        explorer = BRepTools_WireExplorer(self.loop)
+        explorer = BRepTools_WireExplorer(self.occ_wire)
         while explorer.More():
             vertex = explorer.CurrentVertex()
             vertices.append(BRepVertex(vertex))
@@ -62,12 +76,19 @@ class BRepLoop:
     @property
     def edges(self) -> List[BRepEdge]:
         edges = []
-        explorer = BRepTools_WireExplorer(self.loop)
+        explorer = BRepTools_WireExplorer(self.occ_wire)
         while explorer.More():
             edge = explorer.Current()
             edges.append(BRepEdge(edge))
             explorer.Next()
         return edges
+
+    @edges.setter
+    def edges(self, edges: List[BRepEdge]) -> None:
+        builder = BRepBuilderAPI_MakeWire()
+        for edge in edges:
+            builder.Add(edge.occ_edge)
+        self.occ_wire = builder.Wire()
 
     @classmethod
     def from_edges(cls, edges: List[BRepEdge]) -> "BRepLoop":
@@ -83,10 +104,9 @@ class BRepLoop:
         ``BRepLoop``
 
         """
-        builder = BRepBuilderAPI_MakeWire()
-        for edge in edges:
-            builder.Add(edge.edge)
-        return cls(builder.Wire())
+        loop = cls()
+        loop.edges = edges
+        return loop
 
     @classmethod
     def from_polyline(cls, polyline: Polyline) -> "BRepLoop":
@@ -106,7 +126,9 @@ class BRepLoop:
         for a, b in pairwise(polyline.points):
             edge = BRepEdge.from_point_point(a, b)
             edges.append(edge)
-        return cls.from_edges(edges)
+        loop = cls()
+        loop.edges = edges
+        return loop
 
     @classmethod
     def from_polygon(cls, polygon: Polygon) -> "BRepLoop":
@@ -126,7 +148,9 @@ class BRepLoop:
         for a, b in pairwise(polygon.points):
             edge = BRepEdge.from_point_point(a, b)
             edges.append(edge)
-        return cls.from_edges(edges)
+        loop = cls()
+        loop.edges = edges
+        return loop
 
     def is_valid(self) -> bool:
         """Verify that the loop is valid.
@@ -136,7 +160,7 @@ class BRepLoop:
         bool
 
         """
-        return brepalgo_IsValid(self.loop)
+        return brepalgo_IsValid(self.occ_wire)
 
     def fix(self) -> None:
         """Try to fix the loop.
@@ -146,6 +170,6 @@ class BRepLoop:
         None
 
         """
-        fixer = ShapeFix_Wire(self.loop)
+        fixer = ShapeFix_Wire(self.occ_wire)
         fixer.Perform()
-        self.loop = fixer.Wire()
+        self.occ_wire = fixer.Wire()
