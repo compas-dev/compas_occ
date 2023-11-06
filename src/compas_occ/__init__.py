b0a283656382426c5f2ec4856c00901587f3d8af
tomvanmele <tom.v.mele@gmail.com>
2023-11-06 22:20:36 +0100

    update docstring

--- src/compas_occ/__init__.py
+++ src/compas_occ/__init__.py
@@ -1,20 +1,3 @@
-"""
-********************************************************************************
-compas_occ
-********************************************************************************
-
-.. currentmodule:: compas_occ
-
-
-.. toctree::
-    :maxdepth: 1
-
-    compas_occ.brep
-    compas_occ.conversions
-    compas_occ.geometry
-
-"""
-
 from __future__ import print_function
 
 import os
