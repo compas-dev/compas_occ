49fcf7784a59b050ff7ff82622e565fb9c60a0db
tomvanmele <tom.v.mele@gmail.com>
2024-01-16 22:18:01 +0100

    entirely remove linkcode for now

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ else:
 # -- Extension configuration ------------------------------------------------
 
 extensions = sphinx_compas2_theme.default_extensions
+extensions.remove("sphinx.ext.linkcode")
 
 # numpydoc options
 
