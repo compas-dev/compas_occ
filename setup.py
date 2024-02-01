33c75cdab6fbe6fede900fc7b8e6120e68a4ea63
Li <leochen1990@gmail.com>
2024-02-01 21:14:45 +0100

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ optional_requirements = {}
 
 setup(
     name="compas_occ",
-    version="0.7.1",
+    version="1.0.0",
     description="COMPAS wrapper for the Python bindings of OCC",
     long_description=long_description,
     long_description_content_type="text/markdown",
