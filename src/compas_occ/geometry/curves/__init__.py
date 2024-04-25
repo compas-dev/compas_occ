d47847a703e915a0ae894fe8376551c0c505964d
tomvanmele <tom.v.mele@gmail.com>
2024-04-25 22:18:06 +0200

    i really think it should be like this and in 3.12 it otherwise doesn't work

--- src/compas_occ/geometry/curves/__init__.py
+++ src/compas_occ/geometry/curves/__init__.py
@@ -1,20 +1,25 @@
 from .curve2d import OCCCurve2d  # noqa : F401
-from .curve import OCCCurve
+from .curve import OCCCurve  # noqa: F401
 from .nurbs import OCCNurbsCurve
+
+from compas.geometry import Curve
+from compas.geometry import NurbsCurve
 
 from compas.plugins import plugin
 
 
 @plugin(category="factories", requires=["compas_occ"])
 def new_curve(cls, *args, **kwargs):
-    curve = object.__new__(OCCCurve)
-    return curve
+    # curve = object.__new__(OCCCurve)
+    # return curve
+    return super(Curve, cls).__new__(cls)
 
 
 @plugin(category="factories", requires=["compas_occ"])
 def new_nurbscurve(cls, *args, **kwargs):
-    curve = object.__new__(OCCNurbsCurve)
-    return curve
+    # curve = object.__new__(OCCNurbsCurve)
+    # return curve
+    return super(NurbsCurve, cls).__new__(cls)
 
 
 @plugin(category="factories", requires=["compas_occ"])
