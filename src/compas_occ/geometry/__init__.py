debc50160542dddd4dc1d7852310602599697ade
tomvanmele <tom.v.mele@gmail.com>
2024-07-06 12:53:01 +0200

    remove for now

--- src/compas_occ/geometry/__init__.py
+++ src/compas_occ/geometry/__init__.py
@@ -3,6 +3,4 @@ from .curves import OCCCurve  # noqa: F401
 from .curves import OCCNurbsCurve  # noqa: F401
 
 from .surfaces import OCCSurface  # noqa: F401
-from .surfaces import OCCExtrusionSurface  # noqa: F401
-from .surfaces import OCCRevolutionSurface  # noqa: F401
 from .surfaces import OCCNurbsSurface  # noqa: F401
